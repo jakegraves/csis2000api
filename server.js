578f8b55683ba437940d3848ac5942c473631b00
Jacob Graves <jake@hashidev.com>
2018-10-01 19:03:09 -0600

    Adds new endpoints.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.json());
 
+app.use(express.static(__dirname + "/public"));
+
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
@@ -76,6 +78,98 @@ app.get("/demo/all", function (req, res) {
 
   for (let i = 0; i < total; i++) {
     array.push(getUserCard(idStart++));
+  }
+
+  res.status(200).json(array);
+
+});
+
+function getTransaction(id) {
+  let transaction = faker.helpers.createTransaction();
+  transaction.amount = `$${transaction.amount}`;
+  transaction.id = id;
+  transaction.purchaseImageUrl = faker.image.imageUrl(300, 300);
+  return transaction;
+}
+
+app.get("/transactions", function (req, res) {
+
+  let total = faker.random.number({max:20, min:2});
+
+  let idStart = faker.random.number();
+
+  let array = [];
+
+  for (let i = 0; i < total; i++) {
+    array.push(getTransaction(idStart++));
+  }
+
+  res.status(200).json(array);
+
+});
+
+function getAccount(id) {
+  let firstName = faker.name.firstName();
+  let lastName = faker.name.lastName();
+
+  return {
+    id,
+    accountName: faker.finance.accountName(),
+    accountNumber: faker.finance.account(),
+    amount: faker.finance.amount(1111, 999999, undefined, "$"),
+    owner: `${firstName} ${lastName}`,
+    ownerPhone: faker.phone.phoneNumberFormat(),
+    ownerEmail: faker.internet.email(firstName, lastName),
+    ownerImageUrl: faker.image.people(300,300)
+  }
+
+}
+
+
+app.get("/accounts", function (req, res) {
+
+  let total = faker.random.number({max:20, min:2});
+
+  let idStart = faker.random.number();
+
+  let array = [];
+
+  for (let i = 0; i < total; i++) {
+    array.push(getAccount(idStart++));
+  }
+
+  res.status(200).json(array);
+
+});
+
+function getComputers(id) {
+  let firstName = faker.name.firstName();
+  let lastName = faker.name.lastName();
+
+  return {
+    id,
+    userName: faker.internet.userName(firstName, lastName),
+    ip: faker.internet.ip(),
+    ipv6: faker.internet.ipv6(),
+    macAddress: faker.internet.mac(),
+    userAgent: faker.internet.userAgent(),
+    password: faker.internet.password(),
+    locationUrl: faker.image.city(300,300)
+  }
+
+}
+
+
+app.get("/computers", function (req, res) {
+
+  let total = faker.random.number({max:20, min:2});
+
+  let idStart = faker.random.number();
+
+  let array = [];
+
+  for (let i = 0; i < total; i++) {
+    array.push(getComputers(idStart++));
   }
 
   res.status(200).json(array);
